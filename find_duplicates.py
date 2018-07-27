3eb8f31648f53220b9d58704bb5d1055e1ff7fc8
Patty <pattyjula@gmail.com>
2018-07-27 14:17:59 -0700

    initial commit

---
+++ find_duplicates.py
@@ -0,0 +1,44 @@
+'''
+DESC: Creates a CSV file of duplicate records
+CAUTION: User must update CSV files and key_field values
+AUTHOR: Patty Jula
+DATE: July 2018
+'''
+
+from __future__ import print_function
+import sys
+from datetime import datetime
+import pandas as pd
+
+# Print start time
+print("Start time: " + str(datetime.now()))
+# Set variables
+basepath= 'C:/Users/N5875/GIS/Scripts/OpenData/'
+file_in = basepath +"input/LAPD_Calls_for_Service_2018.csv"
+file_out = basepath + 'output/CFS_dupes.csv'
+# key field value
+field_name ="Incident_Number"
+# read in CSV
+df = pd.read_csv(file_in)
+key_field = set(df[field_name].values)
+header = True
+print(len(df))
+
+try:
+
+    ids = df[field_name]
+    data = pd.concat(g for _, g in df.groupby(field_name) if len(g) > 1) 
+    print(len(data))
+    if len(data) > 1:
+        data.to_csv(file_out, mode='w', header=header, index=False)
+        #header = False
+
+    else:
+        pass
+
+    print("End time: " + str(datetime.now()))
+
+except:
+    # print error if there's a problem
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
---
+++ remove_duplicates.py
@@ -0,0 +1,55 @@
+'''
+DESC: Remove duplicates from file_in
+CAUTION: User must update CSV files and key_field values
+AUTHOR: Patty Jula
+DATE: July 2018
+'''
+from __future__ import print_function
+import sys
+from datetime import datetime
+import time
+import pandas as pd
+import csv
+from pathlib import Path
+
+print("Start time: " + str(datetime.now()))
+timestr = time.strftime("%Y%m%d")
+basepath= 'C:/Users/N5875/GIS/Scripts/OpenData'
+file_in = basepath + '/input/' + "LAPD_Calls_for_Service_2018.csv"
+
+
+try:
+    # Read in CSV
+    df= pd.read_csv(file_in)
+
+    # Check if duplicate file is found
+    # before running to make clean file
+    file_out2 = Path(basepath + '/output/' + 'CFS_dupes.csv')
+    if file_out2.is_file():
+        print("Duplicates found")
+        file_out = basepath + '/output/' + 'LAPD_Calls_for_Service_2018_clean.csv'
+        with open(file_in, 'r') as fin, open(file_out, 'w', newline='') as fout:
+            reader = csv.reader(fin)
+            writer = csv.writer(fout)
+            d = {}
+            for row in reader:
+                DR = row[0]
+                if DR not in d:
+                    d[DR] = row
+                    writer.writerow(row)
+                elif DR in d:
+                    continue
+                else:
+                    continue
+        result = d.values()
+        df= pd.read_csv(file_out)
+        
+        print('Clean output CSV count:',len(df))
+    else:
+        print("No duplicates found")
+        pass
+
+except:
+    # print error if there's a problem
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
